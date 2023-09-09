752f9ecb462f1684adf5a8902e846d6db0bb6a03
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-09-09 00:00:16 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { AnimatePresence, motion, spring } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import NewChallenge from "./NewChallenge.jsx";
 
