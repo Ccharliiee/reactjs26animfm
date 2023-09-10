e70525b5a5df18ac8953d6ed8ed129e47cdb7539
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-09-10 19:36:16 -0400

    add animation for count number change

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,3 +1,13 @@
+import { motion } from "framer-motion";
+
 export default function Badge({ caption }) {
-  return <span className="badge">{caption}</span>;
+  return (
+    <motion.span
+      className="badge"
+      animate={{ scale: [1, 1.25, 1] }}
+      transition={{ duration: 0.25 }}
+    >
+      {caption}
+    </motion.span>
+  );
 }
